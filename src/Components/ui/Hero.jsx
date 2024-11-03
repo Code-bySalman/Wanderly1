58916e79397038c9e5f7bd4f1b332eff8ecf8d46
Code-bySalman <usmanis@student.iul.ac.in>
2024-11-03 20:16:08 +0530

    kjhjsd

--- src/Components/ui/Hero.jsx
+++ src/Components/ui/Hero.jsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 
 function Hero() {
   return (
+    <>
     <div className="flex flex-col items-center mx-auto gap-5 pl-10 md:pl-20">  {/* Added left margin */}
       <div className="flex flex-col items-center md:flex-row">
         <div className="text-center md:text-left md:w-1/2">
-          <h1 className="font-bold text-[30px] md:text-[40px]">
+          <h1 className="font-bold text-[30px] md:text-[40px] mt-10">
             Discover the world effortlessly with <span className="text-blue-600">Wanderly</span> – your AI-powered trip planner for seamless, personalized adventures!
           </h1>
           <h4 className="font-normal text-[15px] md:text-[18px] text-gray-700 mt-2">
@@ -19,11 +20,14 @@ function Hero() {
             <Button>Get Started, It's Free!!</Button>
           </Link>
         </div>
-        <div className="md:w-1/2">
-          <img className="rounded-full mb-35 md:mb-0 h-[600px] md:h-[500px] mt-10" src="/hero1.png" alt="Hero Image" />
-        </div>
+        
+          
+        
       </div>
+      
     </div>
+    <img className="rounded-md mb-35 md:mb-0 h-[300px] md:h-[500px] ml-[820px] mt-[-430px] " src="/hero1.png" alt="Hero Image" />
+    </>
   );
 }
 
